96561ce67a81dc238c09d4ce2c8f5c65dff3f667
SalmaAlbakr <salalbakr19988@gmail.com>
2023-05-29 01:08:36 +0300

    commit

--- lib/view/specialist_screen.dart
+++ lib/view/specialist_screen.dart
@@ -12,32 +12,61 @@ class SpecialistScreen extends StatelessWidget {
         child: Column(
           children: [
             ListTile(
-              leading: Image.asset(""),
+              leading: Image.asset(AppImages.logo),
               title: Text("The Name"),
               subtitle: Text("Specialist Doctor"),
-              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.notifications_none_outlined)),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_none_outlined,
+                ),
+              ),
             ),
             Row(
               children: [
                 Column(
                   children: [
-                    SpecialistScreenButton(logo: AppImages.callsLogo, text: "Calls", hintText: "You have new +1 Request",color: Colors.blue,),
-                    SpecialistScreenButton(logo: AppImages.reportsLogo, text: "Reports", hintText: "You have new +3 Request",color: Colors.deepPurpleAccent,),
+                    SpecialistScreenButton(
+                      logo: AppImages.callsLogo,
+                      text: "Calls",
+                      hintText: "You have new +1 Request",
+                      color: Colors.blue,
+                    ),
+                    SpecialistScreenButton(
+                      logo: AppImages.reportsLogo,
+                      text: "Reports",
+                      hintText: "You have new +3 Request",
+                      color: Colors.deepPurpleAccent,
+                    ),
                   ],
                 ),
-                Column(children: [
-                  SpecialistScreenButton(logo: AppImages.tasksLogo, text: "Tasks", hintText: "You have new +1 Task",color: Colors.green,),
-                  SpecialistScreenButton(logo: AppImages.fingerprintLogo, text: "", hintText: "attendance-leaving",color: Colors.cyan,),
-                ],),
+                Column(
+                  children: [
+                    SpecialistScreenButton(
+                      logo: AppImages.tasksLogo,
+                      text: "Tasks",
+                      hintText: "You have new +1 Task",
+                      color: Colors.green,
+                    ),
+                    SpecialistScreenButton(
+                      logo: AppImages.fingerprintLogo,
+                      text: "",
+                      hintText: "attendance-leaving",
+                      color: Colors.cyan,
+                    ),
+                  ],
+                ),
               ],
             ),
-            SpecialistScreenButton(logo: AppImages.casesLogo, text: "Cases", hintText: "",color: Colors.orange[900],),
-
+            SpecialistScreenButton(
+              logo: AppImages.casesLogo,
+              text: "Cases",
+              hintText: "",
+              color: Colors.orange[900],
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
